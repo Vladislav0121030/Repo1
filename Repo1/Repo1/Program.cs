3b09e0608fc4a8098311dd8a7f2e66226f525c42
PIBO0121030 Гуляев Владислав Егорович <PIBO0121030@sibupk.su>
2022-10-08 10:52:02 +0700

    Добавлен файл

---
+++ Repo1/Repo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Repo1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите год ");
+            int? a = Convert.ToInt32(Console.ReadLine());
+            if (a % 4 == 0)
+            {
+                Console.WriteLine("Год вискосный");
+            }
+            else
+            {
+                Console.WriteLine("Год невискосный");
+            }
+        }
+    }
+}
